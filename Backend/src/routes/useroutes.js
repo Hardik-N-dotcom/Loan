bcaf39611c82d155696e3732f282889f35ba7025
Hardik-N-dotcom <hrdknarayan@gmail.com>
2025-07-18 01:08:47 +0530

    routes

--- Backend/src/routes/useroutes.js
+++ Backend/src/routes/useroutes.js
@@ -1,6 +1,6 @@
 import express from "express"
-import { signup,login,allusers, userinfo, logout } from "../../controllers/authcontroller.js"
-import { requireauth } from "../../middlewares/authmiddleware.js"
+import { signup,login,allusers, userinfo, logout } from "../controllers/authcontroller.js"
+import { requireauth } from "../middlewares/authmiddleware.js"
 const router =express.Router()
 
 router.post('/signup',signup)
